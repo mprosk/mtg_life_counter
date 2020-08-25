26dd08afae3f39edd9e1b4883a573250a50faeef
Max Proskauer <mprosk@gmail.com>
2020-08-24 23:36:43 -0400

    Move templates to folder

---
+++ mtg_firmware/templates/c_template.c
@@ -0,0 +1,104 @@
+/***********************************************************************
+ *  Project Reference <<TODO>>
+ *
+ *  DESCRIPTION
+ *      <<TODO>> Tells you what the code in the file does or refers to
+ *      accompanying header file.
+ *
+ *  REFERENCES
+ *      <<TODO>> Requirements Specification
+ *      <<TODO>> Software Specification
+ ***********************************************************************/
+
+/*=====================================================================*
+    Local Header Files
+ *=====================================================================*/
+/* None */
+
+
+/*=====================================================================*
+    Interface Header Files
+ *=====================================================================*/
+/* None */
+
+
+/*=====================================================================*
+    System-wide Header Files
+ *=====================================================================*/
+/* None */
+
+
+/*=====================================================================*
+    Private Defines
+ *=====================================================================*/
+/* None */
+
+
+/*=====================================================================*
+    Private Data Types
+ *=====================================================================*/
+/* None */
+
+
+/*=====================================================================*
+    Private Function Prototypes
+ *=====================================================================*/
+/* None */
+
+
+/*=====================================================================*
+    Private Data
+ *=====================================================================*/
+/* None */
+
+
+/*=====================================================================*
+    Public Function Implementations
+ *=====================================================================*/
+
+/*---------------------------------------------------------------------*
+ *  NAME
+ *      universe_galaxy_system_planet_verb
+ *
+ *  DESCRIPTION
+ *      <<TODO>> Description of the function in as much detail as is necessary
+ *      for a maintainer of the function. This should only include details of
+ *      the implementation that are not obvious from the code & comments within
+ *      the function itself. It should not include information that is conveyed
+ *      in the associated function header in the module's .h file.
+ *
+ *      Examples of content to include:
+ *          - Discussion of algorithms used and data transformed
+ *          - Description of the error handling and recovery (if any)
+ *          - Description of any limitations or unusual features that restrict
+ *            the performance of the function
+ *
+ *      Enclose parameter names referenced in the description in single quotes.
+ *
+ *  RETURNS
+ *      <<TODO>> Describe what data the function returns
+ *---------------------------------------------------------------------*/
+/* Your code goes here */
+
+/*=====================================================================*
+    Private Function Implementations
+
+    GUIDANCE:
+    This section contains the definition (implementation) of functions private
+    to this .c file. Their prototype must be declared in the
+    'Private Function Prototypes' section earlier in this file.
+
+    Headers must be supplied for these function defintions describing what
+    the function does (but not how it does it, i.e. the details of the
+    implementation), its input and output parameters, and its return value
+    (if appropriate).
+
+    RATIONALE:
+    This follows best practices in data-hiding and encapsulation. It allows
+    the module implemenatation to change substantially without affecting the
+    module interface.
+
+    EXAMPLES:
+    See the coding standards or style guidelines for your project.
+ *=====================================================================*/
+/* None */
---
+++ mtg_firmware/templates/h_template.h
@@ -0,0 +1,71 @@
+/***********************************************************************
+ *  Project Reference <<TODO>>
+ *
+ *  DESCRIPTION
+ *      <<TODO>> Tells you what the code in the file does.
+ *
+ *  REFERENCES
+ *      <<TODO>> Requirements Specification
+ *      <<TODO>> Software Specification
+ ***********************************************************************/
+
+#if !defined(INC_<<TODO>>_H)
+#define INC_<<TODO>>_H
+
+
+/*=====================================================================*
+    Required Header Files
+ *=====================================================================*/
+#include <stdint.h>
+
+
+/*=====================================================================*
+    Public Defines
+ *=====================================================================*/
+/* None */
+
+
+/*=====================================================================*
+    Public Data Types
+ *=====================================================================*/
+/* None */
+
+
+/*=====================================================================*
+    Public Data
+ *=====================================================================*/
+/* REMOVE THIS SECTION IF NO PUBLIC DATA IS DEFINED */
+
+
+/*=====================================================================*
+    Public Functions
+ *=====================================================================*/
+
+/*---------------------------------------------------------------------*
+ *  NAME
+ *      universe_galaxy_system_planet_verb
+ *
+ *  DESCRIPTION
+ *      <<TODO>> Description of the function in as much detail as is necessary
+ *      for the caller of the function. As much as possible, this should be
+ *      agnostic of any aspect of the function's implementation. This allows
+ *      the implementation to change, if needed, without modifying the API
+ *      without changing the externally-visible behaviour and associated tests.
+ *
+ *      This should include:
+ *          - Pre-conditions to be satisfied before the function can be called
+ *          - Post-conditions that must be satisfied when the function returns
+ *          - Description of any constraints, limitations, or unusual aspects
+ *            of the function the caller must be aware of, such as:
+ *              - blocking/non-blocking behaviour
+ *              - side-effects
+ *              - MT-safety
+ *              - required calling context or periodicity
+ *
+ *      Enclose parameter names referenced in the description in single quotes.
+ *
+ *  RETURNS
+ *      <<TODO>> Describe what data the function returns
+ *---------------------------------------------------------------------*/
+
+#endif /* !defined(INC_<<TODO>>_H) */
