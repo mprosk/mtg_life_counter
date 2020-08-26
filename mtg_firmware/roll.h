72dfaf66b2af657f5638ed1729916b3c8f72f19a
Max Proskauer <mprosk@gmail.com>
2020-08-25 21:48:27 -0400

    Begin roll refactor

---
+++ mtg_firmware/roll.h
@@ -0,0 +1,57 @@
+/***********************************************************************
+ *  MTG Life Counter - Roll Button Engine
+ *
+ *  DESCRIPTION
+ *      This module provides functions for enabling the roll feature
+ *      of the counter. This feature allows players to randomly select
+ *      one of the players (for randomly select first player or 
+ *      damage targets). The result of the roll is shown on the display
+ *
+ *  REFERENCES
+ *      None
+ ***********************************************************************/
+
+#if !defined(INC_ROLL_H)
+#define INC_ROLL_H
+
+
+/*=====================================================================*
+    Required Header Files
+ *=====================================================================*/
+#include <arduino.h>
+
+
+/*=====================================================================*
+    Public Defines
+ *=====================================================================*/
+/* None */
+
+
+/*=====================================================================*
+    Public Data Types
+ *=====================================================================*/
+/* None */
+
+
+/*=====================================================================*
+    Public Data
+ *=====================================================================*/
+/* REMOVE THIS SECTION IF NO PUBLIC DATA IS DEFINED */
+
+
+/*=====================================================================*
+    Public Functions
+ *=====================================================================*/
+
+/*---------------------------------------------------------------------*
+ *  NAME
+ *      universe_galaxy_system_planet_verb
+ *
+ *  DESCRIPTION
+ *      <<TODO>> Description of the function in as much detail as is necessary
+ *
+ *  RETURNS
+ *      <<TODO>> Describe what data the function returns
+ *---------------------------------------------------------------------*/
+
+#endif /* !defined(INC_<<TODO>>_H) */
